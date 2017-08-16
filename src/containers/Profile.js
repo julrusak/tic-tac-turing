c4bf36f138535698013c3ad006d6af36fc3c6e48
julrusak <juliannarusak@gmail.com>
2017-08-16 17:49:16 -0400

    Initial styled Profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,13 +1,59 @@
 import React, {Component} from 'react'
-
+import {Container, Name, GameListHeader, GameList, GameRecord, Column, ColumnLabels} from '../styled/Profile'
 
 class Profile extends Component {
 
+	static defaultProps = {
+		user: {
+			email: 'useremail',
+			games: [
+				{
+					winner: true,
+					createdAt: '12/12/2016',
+					id: '0001'
+				},
+				{
+					winner: true,
+					createdAt: '12/15/2016',
+					id: '0002'
+				},
+				{
+					winner: false,
+					createdAt: '12/18/2016',
+					id: '0003'
+				}
+			]
+		}
+	}
+
 	render() {
+		let {email} = this.props.user
 		return (
-			<div>
-				<h2>Profile!</h2>
-			</div>
+			<Container>
+				<Name>
+					{email}
+				</Name>
+				<GameList>
+					<GameListHeader>
+						MyGames
+					</GameListHeader>
+					<ColumnLabels>
+						<Column>
+							Outcome
+						</Column>
+						<Column>
+							Guess
+						</Column>
+						<Column>
+							Guessed Correctly?
+						</Column>
+						<Column>
+							Date
+						</Column>
+
+					</ColumnLabels>
+				</GameList>
+			</Container>
 		)
 	}
 }
